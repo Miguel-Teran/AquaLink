11c5c7e71925d66b7db49455337db12e70183601
JavierT-T <javierantoniot2a@gmail.com>
2025-08-18 10:25:19 -0700

    Clase y registro

---
+++ CLSRegistro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAquaLink
+{
+    internal class CLSRegistro
+    {
+        private int usuarioID;
+        private string contraseña;
+        private string email;
+
+        public CLSRegistro()
+        {
+        }
+
+        public CLSRegistro(int usuarioID, string contraseña, string email)
+        {
+            this.usuarioID = usuarioID;
+            this.contraseña = contraseña;
+            this.email = email;
+        }
+
+        public int UsuarioID { get => usuarioID; set => usuarioID = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
---
+++ Registro_Controlador.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoAquaLink
+{
+    internal class Registro_Controlador
+    {
+        public Registro_Controlador()
+        {
+
+        }
+        public bool AgregarRegistro(CLSRegistro registro)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Conexion.strConexion);
+                if (conn.State == 0)
+                    conn.Open();
+                SqlCommand cmd = new SqlCommand("Agregar_Registro", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UsuarioID", registro.UsuarioID);
+                cmd.Parameters.AddWithValue("@Contraseña", registro.Contraseña);
+                cmd.Parameters.AddWithValue("@Email", registro.Email);
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool ActualizarRegistro(CLSRegistro registro)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(Conexion.strConexion);
+                if (conn.State == 0)
+                    conn.Open();
+                SqlCommand cmd = new SqlCommand("Actualizar_Registro", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@UsuarioID", registro.UsuarioID);
+                cmd.Parameters.AddWithValue("@Contraseña", registro.Contraseña);
+                cmd.Parameters.AddWithValue("@Email", registro.Email);
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+       
+    }
+}
